0592b3d9087dc8ab33eda170e44b44dbba6451a7
Alistair Dutton <kelveden@gmail.com>
2017-10-15 21:44:53 +0100

    Prep for next release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kelveden/clj-wiremock "0.2.0"
+(defproject kelveden/clj-wiremock "1.0.0"
   :description "Clojure bindings for WireMock"
   :url "https://github.com/kelveden/clj-wiremock"
   :license {:name "Eclipse Public License - v 1.0"
