a90bef016b343cf6d8b9a6b737fd7c75d276a852
Alistair Dutton <kelveden@gmail.com>
2017-10-16 21:09:47 +0100

    Updated to use wiremock 2.9.0.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[cheshire "5.8.0"]
                  [clj-http "3.7.0"]
                  [clojure-future-spec "1.9.0-alpha17"]
-                 [com.github.tomakehurst/wiremock "2.8.0"]
+                 [com.github.tomakehurst/wiremock "2.9.0"]
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.4.0"]
                  [org.slf4j/slf4j-api "1.7.25"]]
