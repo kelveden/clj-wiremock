466b72b545a3a3c55a06c8487557693dd4630357
Alistair Dutton <ald@juxt.pro>
2022-03-29 09:51:16 +0100

    Bump dependencies to latest.
    Closes #6.

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject kelveden/clj-wiremock "1.7.0"
+(defproject kelveden/clj-wiremock "1.8.0"
   :description "Clojure bindings for WireMock"
   :url "https://github.com/kelveden/clj-wiremock"
   :license {:name "Eclipse Public License - v 1.0"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.10.0"]
-                 [clj-http "3.10.0"]
-                 [com.github.tomakehurst/wiremock "2.26.3"]
-                 [org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.logging "1.0.0"]
-                 [org.slf4j/slf4j-api "1.7.30"]]
+  :dependencies [[cheshire "5.10.2"]
+                 [clj-http "3.12.3"]
+                 [com.github.tomakehurst/wiremock "2.27.2"]
+                 [org.clojure/clojure "1.11.0"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.slf4j/slf4j-api "1.7.36"]]
   :jvm-opts ["-Dclojure.spec.check-asserts=true"]
-  :profiles {:dev {:dependencies [[metosin/ring-http-response "0.9.1"]
-                                  [org.slf4j/slf4j-simple "1.7.30"]
+  :profiles {:dev {:dependencies [[metosin/ring-http-response "0.9.3"]
+                                  [org.slf4j/slf4j-simple "1.7.36"]
                                   [slingshot "0.12.2"]]
                    :source-paths ["dev"]
                    :repl-options {:init-ns user}}})
